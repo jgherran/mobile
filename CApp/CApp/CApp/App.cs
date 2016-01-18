62817997492a3d2caa49cfb1cd721bec4d1b6aa3
jgherran <josegherran@hotmail.com>
2016-01-17 21:26:53 -0500

    1

---
+++ CApp/CApp/CApp/App.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace CApp
+{
+	public class App : Application
+	{
+		public App ()
+		{
+            // The root page of your application
+            MainPage = new SimpleContentPage();
+                        
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}

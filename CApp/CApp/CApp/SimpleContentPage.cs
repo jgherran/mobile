83c94a36b0ca9e0738765f83ec57e69ccffb8733
jgherran <josegherran@hotmail.com>
2016-01-17 21:27:27 -0500

    1

---
+++ CApp/CApp/CApp/SimpleContentPage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace CApp
+{
+    public class SimpleContentPage : ContentPage
+    {
+        public SimpleContentPage()
+        {
+            var labTitulo = new Label
+               {
+                   Text = "Registro",
+                   FontSize = 24
+               };
+            var entNombre = new Entry
+            {
+                Placeholder = "Cual es tu problema?"
+            };
+            var entDesc = new Entry
+            {
+                Placeholder = "Describe tu problema..."
+               
+            };
+            var btnEnviar = new Button
+            {
+                Text = "Enviar"
+            };
+
+
+            Content = new StackLayout
+            {
+                Padding = 30,
+                Spacing = 10,
+                Children = { labTitulo, entNombre, entDesc, btnEnviar }
+            };
+
+
+        }
+
+        private Boolean BtnEnviar_Clicked(object sender, EventArgs e)
+        {
+            return true;
+        }
+    }
+}
